ca177fb5a74ea29bc7aa5e37d85bbf73ab119489
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2024-12-18 09:27:04 -0300

    add struct abstraction for clarity

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,58 @@ use macroquad::prelude::*;
 
 mod conways;
 
-const CELL_SIZE: f32 = 50.0;
+const CELL_SIZE: f32 = 10.0;
 const UPDATE_INTERVAL: f64 = 1.0;
 
-fn draw(conways: &Conways, width: usize, height: usize) {
-    clear_background(BLACK);
-    for x in  0..height {
-        for y in 0..width {
-            if conways.is_alive(x,y) {
-                draw_rectangle(x as f32 * CELL_SIZE, y as f32 * CELL_SIZE, CELL_SIZE, CELL_SIZE, WHITE);
+struct Game {
+    conways: Conways,
+    previous_time: f64,
+    width: usize,
+    height: usize
+}
+
+impl Game {
+    pub fn new(width: usize, height: usize) -> Game {
+        Game {
+            conways: Conways::new(width, height),
+            previous_time: get_time(),
+            width,
+            height
+        }
+    }
+
+    pub fn update(&mut self) {
+        let time_of_last_frame = get_time();
+        self.draw();
+        if time_of_last_frame - self.previous_time > UPDATE_INTERVAL {
+            self.conways.tick();
+            self.previous_time = time_of_last_frame;
+        }
+    }
+    fn draw(&self) {
+        clear_background(BLACK);
+        for x in  0..self.height {
+            for y in 0..self.width {
+                if self.conways.is_alive(x,y) {
+                    draw_rectangle(x as f32 * CELL_SIZE, y as f32 * CELL_SIZE, CELL_SIZE, CELL_SIZE, WHITE);
+                }
             }
         }
     }
 }
 
+
 #[macroquad::main("Conways")]
 async fn main() {
-    let mut conway = Conways::new(6, 6);
-    conway.set_alive(1,0);
-    conway.set_alive(1,1);
-    conway.set_alive(1,2);
-
     let width = 50;
     let height = 50;
 
-    let mut previous_time = get_time();
-    draw(&conway, width, height);
+    let mut game = Game::new(width, height);
+    game.conways.set_alive(1, 1);
+    game.conways.set_alive(1, 0);
+    game.conways.set_alive(1, 2);
     loop {
-        let time_of_last_frame = get_time();
-        if time_of_last_frame - previous_time > UPDATE_INTERVAL {
-            draw(&conway, width, height);
-            conway.tick();
-            next_frame().await;
-            previous_time = time_of_last_frame;
-        }
+        game.update();
+        next_frame().await;
     }
 }
